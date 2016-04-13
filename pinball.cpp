75f2ad1b9844cbd670512cb8001f0c2be18f29cb
Omar O <omaroseguera92@gmail.com>
2016-04-13 12:19:20 -0700

    added a second flipper

--- pinball.cpp
+++ pinball.cpp
@@ -1,4 +1,6 @@
 //cs335 Sample
+//NOTE: This program contains to flippers for pinball game
+
 //This program demonstrates billiard ball collisions
 //
 //program: bump.cpp
@@ -33,7 +35,7 @@
 //#include <GL/glu.h>
 //#include "log.h"
 extern "C" {
-	#include "fonts.h"
+#include "fonts.h"
 }
 
 typedef double Flt;
@@ -49,55 +51,55 @@ double DegreesToRadians(double a) {return a * M_PI / 180.0;}
 //Vector Functions
 double VecMagnitude(Vec vec)
 {
-    return sqrt(VecDot(vec, vec));
-    
+	return sqrt(VecDot(vec, vec));
+
 }
 
 double VecProject(Vec vec1, Vec vec2)
 {
-    return VecDot(vec1, vec2) / VecMagnitude(vec2);
+	return VecDot(vec1, vec2) / VecMagnitude(vec2);
 }
 
 double VecAngleBtn(Vec vec1, Vec vec2)
 {
-    return RadiansToDegrees(acos(VecDot(vec1, vec2) / (VecMagnitude(vec1) * VecMagnitude(vec2))));
+	return RadiansToDegrees(acos(VecDot(vec1, vec2) / (VecMagnitude(vec1) * VecMagnitude(vec2))));
 }
 
 void VecScale(Vec vec, double scale, Vec out)
 {
-    out[0] = vec[0] * scale;
-    out[1] = vec[1] * scale;
+	out[0] = vec[0] * scale;
+	out[1] = vec[1] * scale;
 }
 
 void VecRotate(Vec vec, double angle, Vec out)
 {
-    angle = DegreesToRadians(angle);
-    float x = vec[0], y = vec[1];
-    out[0] = x * cos(angle) - y * sin(angle);
-    out[1] = x * sin(angle) + y * cos(angle);
+	angle = DegreesToRadians(angle);
+	float x = vec[0], y = vec[1];
+	out[0] = x * cos(angle) - y * sin(angle);
+	out[1] = x * sin(angle) + y * cos(angle);
 }
 void VecAdd(Vec a, Vec b, Vec out)
 {
-    out[0] = a[0] + b[0];
-    out[1] = a[1] + b[1];
-    out[2] = a[2] + b[2];
+	out[0] = a[0] + b[0];
+	out[1] = a[1] + b[1];
+	out[2] = a[2] + b[2];
 }
 
 void VecBtn(Vec a, Vec b, Vec out)
 {
-    out[0] = b[0] - a[0];
-    out[1] = b[1] - a[1];
-    out[2] = b[2] - a[2];
+	out[0] = b[0] - a[0];
+	out[1] = b[1] - a[1];
+	out[2] = b[2] - a[2];
 }
 
 
 typedef struct t_flipper {
-    Vec pos;
-    Flt angle;
-    Flt rvel;
-    //rotational velocity; gets added to angle every frame
-    int flipstate;
-    //0 - idle, 1 - going up, 2 - going down
+	Vec pos;
+	Flt angle;
+	Flt rvel;
+	//rotational velocity; gets added to angle every frame
+	int flipstate;
+	//0 - idle, 1 - going up, 2 - going down
 } Flipper;
 
 typedef struct t_Ball {
@@ -117,7 +119,7 @@ Window win;
 void initXWindows(void);
 void initOpengl(void);
 void initBalls(void);
-void initFlipper(void);
+void initFlipper(Flipper &f);
 void cleanupXWindows(void);
 void checkResize(XEvent *e);
 void checkMouse(XEvent *e);
@@ -137,6 +139,7 @@ Vec leftButtonPos;
 Ball ball1;
 Ball ball2;
 Flipper flipper;
+Flipper flipper2;
 
 //-----------------------------------------------------------------------------
 //Setup timers
@@ -148,7 +151,7 @@ double physicsCountdown=0.0;
 double timeSpan=0.0;
 double timeDiff(struct timespec *start, struct timespec *end) {
 	return (double)(end->tv_sec - start->tv_sec ) +
-			(double)(end->tv_nsec - start->tv_nsec) * oobillion;
+		(double)(end->tv_nsec - start->tv_nsec) * oobillion;
 }
 void timeCopy(struct timespec *dest, struct timespec *source) {
 	memcpy(dest, source, sizeof(struct timespec));
@@ -161,7 +164,10 @@ int main(void)
 	initXWindows();
 	initOpengl();
 	initBalls();
-    initFlipper();
+	initFlipper(flipper);
+	initFlipper(flipper2);
+	flipper2.pos[0] = 200;
+	flipper2.pos[1] = 100;
 	clock_gettime(CLOCK_REALTIME, &timePause);
 	clock_gettime(CLOCK_REALTIME, &timeStart);
 	while(!done) {
@@ -233,16 +239,16 @@ void initXWindows(void)
 	cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
 	swa.colormap = cmap;
 	swa.event_mask = ExposureMask | KeyPressMask |
-	                 KeyReleaseMask | PointerMotionMask |
-	                 ButtonPressMask | ButtonReleaseMask |
-	                 StructureNotifyMask | SubstructureNotifyMask;
+		KeyReleaseMask | PointerMotionMask |
+		ButtonPressMask | ButtonReleaseMask |
+		StructureNotifyMask | SubstructureNotifyMask;
 	win = XCreateWindow(dpy, root, 0, 0, xres, yres, 0,
-							vi->depth, InputOutput, vi->visual,
-							CWColormap | CWEventMask, &swa);
+			vi->depth, InputOutput, vi->visual,
+			CWColormap | CWEventMask, &swa);
 	GLXContext glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
 	glXMakeCurrent(dpy, win, glc);
 	setTitle();
-    XAutoRepeatOn(dpy);
+	XAutoRepeatOn(dpy);
 }
 
 void reshapeWindow(int width, int height)
@@ -290,14 +296,14 @@ void initBalls(void)
 	ball2.mass = 1.0;
 }
 
-void initFlipper(void)
+void initFlipper(Flipper &f)
 {
-    flipper.pos[0] = 0;
-    flipper.pos[1] = 200;
+	f.pos[0] = 0;
+	f.pos[1] = 200;
 
-    flipper.angle = FLIPPER_REST_ANGLE;
-    flipper.flipstate = 0;
-    flipper.rvel = 0;
+	f.angle = FLIPPER_REST_ANGLE;
+	f.flipstate = 0;
+	f.rvel = 0;
 }
 
 void checkResize(XEvent *e)
@@ -348,10 +354,10 @@ void checkMouse(XEvent *e)
 
 void checkKeys(XEvent *e)
 {
-    int key = XLookupKeysym(&e->xkey, 0);
+	int key = XLookupKeysym(&e->xkey, 0);
 
-    //XEvent nev;
-    //XPeekEvent(dpy, &nev);
+	//XEvent nev;
+	//XPeekEvent(dpy, &nev);
 	//Was there input from the keyboard?
 	if (e->type == KeyPress) {
 		switch(key) {
@@ -376,32 +382,40 @@ void checkKeys(XEvent *e)
 				break;
 			case XK_f:
 				//press s to slow the balls
-                flipper.flipstate = 1;
+				flipper.flipstate = 1;
+				break;
+			case XK_k:
+				//flipper 2
+				flipper2.flipstate = 1;
 				break;
 			case XK_Escape:
 				done=1;
 				break;
 		}
 	}
-    else if (e->type == KeyRelease)
-    {
-            char keys[32];
-            XQueryKeymap(dpy, keys);
+	else if (e->type == KeyRelease)
+	{
+		char keys[32];
+		XQueryKeymap(dpy, keys);
 
-            if(!(keys[e->xkey.keycode>>3] & (0x1 << (e->xkey.keycode % 8))))
-            {
-                // Stuff to do on KeyRelease
-                switch (key) {
-    			    case XK_f:
-    				    //press s to slow the balls
-                        flipper.flipstate = 2;
-    				    break;
-                }
-            }
-        //if (nev.type != KeyPress || nev.xkey.time != e->xkey.time ||
-        //    nev.xkey.keycode != e->xkey.keycode) {
-        //}
-    }
+		if(!(keys[e->xkey.keycode>>3] & (0x1 << (e->xkey.keycode % 8))))
+		{
+			// Stuff to do on KeyRelease
+			switch (key) {
+				case XK_f:
+					//press s to slow the balls
+					flipper.flipstate = 2;
+					break;
+				case XK_k:
+					//flipper 2
+					flipper2.flipstate = 2;
+					break;
+			}
+		}
+		//if (nev.type != KeyPress || nev.xkey.time != e->xkey.time ||
+		//    nev.xkey.keycode != e->xkey.keycode) {
+		//}
+	}
 }
 
 void VecNormalize(Vec v)
@@ -419,48 +433,49 @@ void VecNormalize(Vec v)
 //moves the flipper based on its flipstate
 void flipperMovement(Flipper &f)
 {
-    //add rotational velocity to angle
-    f.angle += f.rvel;
+	//add rotational velocity to angle
+	f.angle += f.rvel;
 
-    switch (f.flipstate)
-    {
-        //idle; rotational velocity is 0 and the angle is the rest angle
-        case 0:
-            f.rvel = 0;
-            f.angle = FLIPPER_REST_ANGLE;
-            break;
-        case 1:
-        //going up; set rotational velocity to flipper speed until the flipper passes the max angle
-            if (f.angle < 40) {
-                f.rvel = FLIPPER_SPEED;
-            }
-            else {
-        // if the flipper is passed the max angle, set flipstate to 'going down'
-                f.rvel = 0;
-                //f.flipstate = 2;
-            }
-            break;
-        case 2: 
-        //going down; make rotational velocity negative until flipper is at rest angle
-            if (f.angle > FLIPPER_REST_ANGLE) {
-                f.rvel = -10;
-            }
-            else {
-                f.rvel = 0;
-                f.flipstate = 0;
-            }
-            break;
-    }
+	switch (f.flipstate)
+	{
+		//idle; rotational velocity is 0 and the angle is the rest angle
+		case 0:
+			f.rvel = 0;
+			f.angle = FLIPPER_REST_ANGLE;
+			break;
+		case 1:
+			//going up; set rotational velocity to flipper speed until the flipper passes the max angle
+			if (f.angle < 40) {
+				f.rvel = FLIPPER_SPEED;
+			}
+			else {
+				// if the flipper is passed the max angle, set flipstate to 'going down'
+				f.rvel = 0;
+				//f.flipstate = 2;
+			}
+			break;
+		case 2: 
+			//going down; make rotational velocity negative until flipper is at rest angle
+			if (f.angle > FLIPPER_REST_ANGLE) {
+				f.rvel = -10;
+			}
+			else {
+				f.rvel = 0;
+				f.flipstate = 0;
+			}
+			break;
+	}
 }
 
 void physics(void)
 {
-
-    flipperMovement(flipper);
-    
-    flipperBallCollision(flipper, ball1);
-    flipperBallCollision(flipper, ball2);
-/*
+	//flipper physics
+	flipperMovement(flipper);
+	flipperBallCollision(flipper, ball1);
+	//flipper2 physics
+	flipperMovement(flipper2);
+	flipperBallCollision(flipper2, ball1);
+	/*
 	//check for collision here
 	Flt distance, speed;
 	Flt tx, ty;
@@ -473,48 +488,48 @@ void physics(void)
 	ty = ball1.pos[1] - ball2.pos[1];
 	distance = sqrt(tx * tx + ty * ty);
 	if (distance < (ball1.radius+ball2.radius)) {
-		//We have a collision!
-		//vector from center to center.
-		vcontact[0][0] = tx;
-		vcontact[0][1] = ty;
-		vcontact[0][2] = 0.0;
-		VecNormalize(vcontact[0]);
-		VecCopy(vcontact[0],vcontact[1]);
-		VecNegate(vcontact[1]);
-		movi[0] = ball1.vel[0];
-		movi[1] = ball1.vel[1];
-		movj[0] = ball2.vel[0];
-		movj[1] = ball2.vel[1];
-		vmove[0][0] = movi[0];
-		vmove[0][1] = movi[1];
-		vmove[0][2] = 0.0;
-		vmove[1][0] = movj[0];
-		vmove[1][1] = movj[1];
-		vmove[1][2] = 0.0;
-		VecNormalize(vmove[0]);
-		VecNormalize(vmove[1]);
-		//Determine how direct the hit was...
-		dot0 = VecDot(vcontact[0], vmove[0]);  //dot product
-		dot1 = VecDot(vcontact[1], vmove[1]);  //dot product
-		//Find the closing (relative) speed of the objects...
-		Flt speed0 = sqrtf( movi[0]*movi[0] + movi[1]*movi[1]) * dot0;
-		Flt speed1 = sqrtf( movj[0]*movj[0] + movj[1]*movj[1]) * dot1;
-		speed = speed0 + speed1;
-		if (speed < 0.0) {
-			//Normalize the mass of each object...
-			massFactor = 2.0 / (ball1.mass + ball2.mass);
-			massi = ball1.mass * massFactor;
-			massj = ball2.mass * massFactor;
-			ball2.vel[0] += vcontact[0][0] * speed * massi;
-			ball2.vel[1] += vcontact[0][1] * speed * massi;
-			ball1.vel[0] += vcontact[1][0] * speed * massj;
-			ball1.vel[1] += vcontact[1][1] * speed * massj;
-		}
+	//We have a collision!
+	//vector from center to center.
+	vcontact[0][0] = tx;
+	vcontact[0][1] = ty;
+	vcontact[0][2] = 0.0;
+	VecNormalize(vcontact[0]);
+	VecCopy(vcontact[0],vcontact[1]);
+	VecNegate(vcontact[1]);
+	movi[0] = ball1.vel[0];
+	movi[1] = ball1.vel[1];
+	movj[0] = ball2.vel[0];
+	movj[1] = ball2.vel[1];
+	vmove[0][0] = movi[0];
+	vmove[0][1] = movi[1];
+	vmove[0][2] = 0.0;
+	vmove[1][0] = movj[0];
+	vmove[1][1] = movj[1];
+	vmove[1][2] = 0.0;
+	VecNormalize(vmove[0]);
+	VecNormalize(vmove[1]);
+	//Determine how direct the hit was...
+	dot0 = VecDot(vcontact[0], vmove[0]);  //dot product
+	dot1 = VecDot(vcontact[1], vmove[1]);  //dot product
+	//Find the closing (relative) speed of the objects...
+	Flt speed0 = sqrtf( movi[0]*movi[0] + movi[1]*movi[1]) * dot0;
+	Flt speed1 = sqrtf( movj[0]*movj[0] + movj[1]*movj[1]) * dot1;
+	speed = speed0 + speed1;
+	if (speed < 0.0) {
+	//Normalize the mass of each object...
+	massFactor = 2.0 / (ball1.mass + ball2.mass);
+	massi = ball1.mass * massFactor;
+	massj = ball2.mass * massFactor;
+	ball2.vel[0] += vcontact[0][0] * speed * massi;
+	ball2.vel[1] += vcontact[0][1] * speed * massi;
+	ball1.vel[0] += vcontact[1][0] * speed * massj;
+	ball1.vel[1] += vcontact[1][1] * speed * massj;
 	}
-*/
+	}
+	 */
 
-    //gravity
-    ball1.vel[1] += -0.1;
+	//gravity
+	ball1.vel[1] += -0.1;
 
 	//Update position
 	ball1.pos[0] += ball1.vel[0];
@@ -526,62 +541,62 @@ void physics(void)
 	}
 	//Check for collision with window edges
 	if ((ball1.pos[0] < ball1.radius && ball1.vel[0] < 0.0) ||
-		(ball1.pos[0] >= (Flt)xres-ball1.radius && ball1.vel[0] > 0.0)) {
+			(ball1.pos[0] >= (Flt)xres-ball1.radius && ball1.vel[0] > 0.0)) {
 		ball1.vel[0] = -ball1.vel[0];
 	}
 	if ((ball1.pos[1] < ball1.radius && ball1.vel[1] < 0.0) ||
-		(ball1.pos[1] >= (Flt)yres-ball1.radius && ball1.vel[1] > 0.0)) {
+			(ball1.pos[1] >= (Flt)yres-ball1.radius && ball1.vel[1] > 0.0)) {
 		ball1.vel[1] = -ball1.vel[1];
 	}
 }
 
 void flipperBallCollision(Flipper &f, Ball &b)
 {
-    //unit axis vectors
-    Vec vert, horz;
-    MakeVector(0, 1, 0, vert);
-    MakeVector(1, 0, 0, horz);
-    //rotated
-    VecRotate(vert, f.angle, vert);
-    VecRotate(horz, f.angle, horz);
+	//unit axis vectors
+	Vec vert, horz;
+	MakeVector(0, 1, 0, vert);
+	MakeVector(1, 0, 0, horz);
+	//rotated
+	VecRotate(vert, f.angle, vert);
+	VecRotate(horz, f.angle, horz);
 
-    Vec between;
-    VecBtn(f.pos, b.pos, between);
-    double projectX = VecProject(between, horz);
-    double projectY = VecProject(between, vert);
+	Vec between;
+	VecBtn(f.pos, b.pos, between);
+	double projectX = VecProject(between, horz);
+	double projectY = VecProject(between, vert);
 
-    //check collision
-    if (projectX > 0 && projectX < FLIPPER_LENGTH + b.radius
-        && projectY > -(b.radius + 20) && projectY < b.radius) {
-        Vec dP;
-        VecScale(vert, b.radius - projectY, dP);
-        VecAdd(b.pos, dP, b.pos);
+	//check collision
+	if (projectX > 0 && projectX < FLIPPER_LENGTH + b.radius
+			&& projectY > -(b.radius + 20) && projectY < b.radius) {
+		Vec dP;
+		VecScale(vert, b.radius - projectY, dP);
+		VecAdd(b.pos, dP, b.pos);
 
-        Vec dV;
-        double speed;
+		Vec dV;
+		double speed;
 
-        //speed is based on the incoming velocity
-        //the horizontal distance from the pivot point of the flipper (torque)
-        //and the speed at which the flipper is rotating 
-        speed = 0.5 * VecMagnitude(b.vel) + pow((projectX / FLIPPER_LENGTH) + 0.5, 4) * (f.rvel / 10);
-        VecScale(vert, speed, dV);
-        VecAdd(b.vel, dV, b.vel);
+		//speed is based on the incoming velocity
+		//the horizontal distance from the pivot point of the flipper (torque)
+		//and the speed at which the flipper is rotating 
+		speed = 0.5 * VecMagnitude(b.vel) + pow((projectX / FLIPPER_LENGTH) + 0.5, 4) * (f.rvel / 10);
+		VecScale(vert, speed, dV);
+		VecAdd(b.vel, dV, b.vel);
 
-        //reflection vector
-        VecRotate(dV, VecAngleBtn(between, vert), dV); 
-        
-        
-        //add horizontal velocity if at the tip of flipper
-       /* if (projectX > FLIPPER_LENGTH - 5.0)
-        {
-            VecScale(horz, speed * 0.4, dV);
-            VecAdd(b.vel, dV, b.vel);
-        }*/
-        
-        
-    }
-    
-    
+		//reflection vector
+		VecRotate(dV, VecAngleBtn(between, vert), dV); 
+
+
+		//add horizontal velocity if at the tip of flipper
+		/* if (projectX > FLIPPER_LENGTH - 5.0)
+		   {
+		   VecScale(horz, speed * 0.4, dV);
+		   VecAdd(b.vel, dV, b.vel);
+		   }*/
+
+
+	}
+
+
 }
 
 void drawBall(Flt rad)
@@ -601,9 +616,9 @@ void drawBall(Flt rad)
 		firsttime=0;
 	}
 	glBegin(GL_TRIANGLE_FAN);
-		for (i=0; i<n; i++) {
-			glVertex2f(verts[i][0]*rad, verts[i][1]*rad);
-		}
+	for (i=0; i<n; i++) {
+		glVertex2f(verts[i][0]*rad, verts[i][1]*rad);
+	}
 	glEnd();
 }
 
@@ -612,8 +627,8 @@ void render(void)
 {
 	Rect r;
 	glClear(GL_COLOR_BUFFER_BIT);
-	
-    //draw balls
+
+	//draw balls
 	glColor3ub(30,60,90);
 	glPushMatrix();
 	glTranslatef(ball1.pos[0], ball1.pos[1], ball1.pos[2]);
@@ -622,47 +637,36 @@ void render(void)
 
 
 
+
+	//glPopMatrix();
+
+	glPushMatrix();
+	glColor3ub(1, 140, 0);
+	glTranslatef(flipper.pos[0], flipper.pos[1], flipper.pos[2]);
+
+	//draw flipper
+	glRotatef(flipper.angle, 0, 0, 1);
+	glBegin(GL_QUADS);
+	glVertex2f(0, -20.0);
+	glVertex2f(FLIPPER_LENGTH, -20.0);
+	glVertex2f(FLIPPER_LENGTH, 0);
+	glVertex2f(0, 0);
+	glEnd();
+	glPopMatrix();
 	
-    glPopMatrix();
 
-    glColor3ub(0, 0, 0);
-    glPushMatrix();
-    glTranslatef(flipper.pos[0], flipper.pos[1], flipper.pos[2]);
-   
-
-    // debug lines
-   /* Vec between;
-    VecBtn(flipper.pos, ball1.pos, between);
-    double projectX = VecProject(between, horz);
-    double projectY = VecProject(between, vert);
-    
-    VecScale(vert, projectY, vert);
-    VecScale(horz, projectX, horz);
-    
-    glLineWidth(2.5);
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_LINES);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(vert[0], vert[1]);
-    glEnd();
-
-
-    glBegin(GL_LINES);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(horz[0], horz[1]);
-    glEnd();
-    //end debug lines
-*/
-
-    //draw flipper
-    glRotatef(flipper.angle, 0, 0, 1);
-    glBegin(GL_QUADS);
-    glVertex2f(0, -20.0);
-    glVertex2f(FLIPPER_LENGTH, -20.0);
-    glVertex2f(FLIPPER_LENGTH, 0);
-    glVertex2f(0, 0);
-    glEnd();
-    glPopMatrix();
+	//flipper2
+	glPushMatrix();
+	glColor3ub(90, 150, 9);
+	glTranslatef(flipper2.pos[0], flipper2.pos[1], flipper2.pos[2]);
+	glRotatef(flipper2.angle, 0, 0, 4);
+	glBegin(GL_QUADS);
+	glVertex2f(0, -20.0);
+	glVertex2f(FLIPPER_LENGTH, -20.0);
+	glVertex2f(FLIPPER_LENGTH, 0);
+	glVertex2f(0, 0);
+	glEnd();
+	glPopMatrix();
 
 	//
 	r.bot = yres - 20;
