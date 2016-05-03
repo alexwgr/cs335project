ad787a798e50681d5887234f87d42a6836282642
Alexander Rinaldi <arinaldi@cs.csubak.edu>
2016-05-02 19:29:33 -0700

    extra comments

--- alexR.h
+++ alexR.h
@@ -1,4 +1,19 @@
 
+// Author: Alex Rinaldi 
+// CS335 Project 
+
+// Purpose 
+// Provide functions to detect and handle collision between the ball and various objects, 
+// along with functions to add more complex objects to the game board.
+
+
+/* Roles for this project:
+ * Collision detection
+ * Physics and movement 
+ * Special effects 
+ */
+
+
 #ifndef _ALEX_R_H_
 #define _ALEX_R_H_
 
@@ -6,8 +21,6 @@
 #include "gameObjects.h"
 #include <iostream>
 #include <cmath>
-
-
 
 bool isLeft(Vec &, Vec &, Vec &);
 void addCurve(Curve &c, GameBoard &b);
